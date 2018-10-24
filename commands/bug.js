44df96e99ffbf8b1af4fef506f945113c1e62377
Jens Kreutschmann <jens@nephila.de>
2018-10-24 22:41:37 +0200

    Refactored so player-events.js is not needed any more
    This first commit is just dumb copy pasting and minor changes. I would like to
    discuss this code further, because I think the command by itself and readability
    of the code could be improved.

--- commands/bug.js
+++ commands/bug.js
@@ -2,6 +2,49 @@
 
 module.exports = srcPath => {
   const Broadcast = require(srcPath + 'Broadcast');
+  const Logger = require(srcPath + 'Logger');
+
+  function getReportMethod(type) {
+    switch (type) {
+      case 'bug':
+        return Logger.error;
+      case 'typo':
+        return Logger.warn;
+      case 'suggestion':
+      default:
+        return Logger.verbose;
+    }
+  }
+
+  function getFormattedReport(player, type, description) {
+    const header = getReportHeader(player, type, description);
+    const specialized = getSpecializedReport(player, type, description);
+    return `${header}${specialized}`;
+  }
+
+  function getReportHeader(player, type, description) {
+    const now = (new Date()).toISOString();
+    return `REPORT\nType: ${type}\nReported By: ${player.name}\nRoom: ${player.room.title}\nTime: ${now}\nDescription: ${description}\n`;
+  }
+
+  function getSpecializedReport(player, type, description) {
+    const room = player.room;
+    const serializeRoom = room => JSON.stringify({
+      name: room.name,
+      desc: room.description,
+      entities: [...room.items, ...room.players, ...room.npcs].map(ent => ({name: ent.name, id: ent.id, desc: ent.description || '' }))
+    });
+
+    switch (type) {
+      case 'bug':
+        return `PlayerData: ${JSON.stringify(player.serialize())} RoomData: ${serializeRoom(room)}`;
+      case 'typo':
+        return `PlayerInv: ${JSON.stringify(player.inventory.serialize())} RoomData: ${serializeRoom(room)}`;
+      case 'suggestion':
+      default:
+        return '';
+    }
+  }
 
   return {
     usage: 'bug <description>',
@@ -11,13 +54,25 @@ module.exports = srcPath => {
         return Broadcast.sayAt(player, '<b><yellow>Please describe the bug you have found.</yellow></b>');
       }
 
-      // TODO: There's absolutely no reason for this to be an event, just move the event code into the command
-      player.emit('bugReport', {
-        description: args,
-        type: arg0
-      });
+      const description = args;
+      const type = arg0;
 
-      Broadcast.sayAt(player, `<b>Your ${arg0} report has been submitted as:</b>\n${args}`);
+      const reportMethod = getReportMethod(type);
+      const formattedReport = getFormattedReport(player, type, description);
+
+      reportMethod(formattedReport);
+
+      // TODO: Looks to me like this never worked, but no one ever set reportToAdmin to true
+      // Will leave it here with a todo, because I like the idea
+      // if (Config.get('reportToAdmins')) {
+      //   const message = `Report from ${this.name}: ${description}. See the server logs for more details.`;
+      //   const minRole = type === 'bug'
+      //     ? PlayerRoles.ADMIN
+      //     : PlayerRoles.BUILDER;
+      //   Broadcast.sayAt(new RoleAudience({ minRole, state }), message);
+      // }
+
+      Broadcast.sayAt(player, `<b>Your ${type} report has been submitted as:</b>\n${description}`);
       Broadcast.sayAt(player, '<b>Thanks!</b>');
     }
   };
