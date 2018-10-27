b013939f88d12e34afc346d3bd201ea069cfa7b3
nahilep <nahilep@users.noreply.github.com>
2018-10-27 10:04:45 +0200

    Refactored so player-events.js is not needed any more (#2)
    * Refactored so player-events.js is not needed any more

--- commands/bug.js
+++ commands/bug.js
@@ -1,24 +1,53 @@
 'use strict';
+const { Broadcast, Logger } = require('ranvier');
 
-module.exports = srcPath => {
-  const Broadcast = require(srcPath + 'Broadcast');
+const logMethods = {
+  'bug': Logger.error,
+  'typo' : Logger.warn,
+  'suggestion': Logger.verbose
+};
 
-  return {
-    usage: 'bug <description>',
-    aliases: ['typo', 'suggestion'],
-    command: state => (args, player, arg0) => {
-      if (!args) {
-        return Broadcast.sayAt(player, '<b><yellow>Please describe the bug you have found.</yellow></b>');
-      }
-
-      // TODO: There's absolutely no reason for this to be an event, just move the event code into the command
-      player.emit('bugReport', {
-        description: args,
-        type: arg0
-      });
-
-      Broadcast.sayAt(player, `<b>Your ${arg0} report has been submitted as:</b>\n${args}`);
-      Broadcast.sayAt(player, '<b>Thanks!</b>');
+module.exports = {
+  usage: 'bug <description>',
+  aliases: ['typo', 'suggestion'],
+  command: state => (args, player, arg0) => {
+    if (!args) {
+      return Broadcast.sayAt(player, '<b><yellow>Please describe the bug you have found.</yellow></b>');
     }
-  };
+
+    const description = args;
+    const type = arg0;
+
+    const room = player.room;
+    const roomData = {
+      name: room.name,
+      desc: room.description,
+      entities: [...room.items, ...room.players, ...room.npcs].map(ent => ({name: ent.name, id: ent.id, desc: ent.description || '' }))
+    };
+
+    const roomJSON = JSON.stringify(roomData, null, 2);
+    const playerJSON = JSON.stringify(player.serialize(), null, 2);
+
+    const header = `REPORT\nType: ${type}\nReported By: ${player.name}\nRoom: ${room.title}\nTime: ${(new Date()).toISOString()}\nDescription: ${description}\n`;
+    const details = `PlayerData: ${playerJSON}\nRoomData: ${roomJSON}`;
+
+    const report = `${header}${details}`;
+
+    const logMethod = logMethods[type] || Logger.verbose;
+    logMethod(report);
+
+    Broadcast.sayAt(player, `<b>Your ${type} report has been submitted as:</b>\n${description}`);
+    Broadcast.sayAt(player, '<b>Thanks!</b>');
+
+    // TODO: Looks to me like this never worked, but no one ever set reportToAdmin to true
+    // Will leave it here with a todo, because I like the idea
+    // if (Config.get('reportToAdmins')) {
+    //   const message = `Report from ${this.name}: ${description}. See the server logs for more details.`;
+    //   const minRole = type === 'bug'
+    //     ? PlayerRoles.ADMIN
+    //     : PlayerRoles.BUILDER;
+    //   Broadcast.sayAt(new RoleAudience({ minRole, state }), message);
+    // }
+
+  }
 };
